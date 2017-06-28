d38991e594a14b2602b6f364e6ec870253200d56
NuclearManD <dylanbrophy@gmail.com>
2017-06-28 00:23:50 -0700

    Basic Networking

--- chaintool.py
+++ chaintool.py
@@ -1,4 +1,12 @@
-import time, os, hashlib
+import time, os, hashlib, threading, socket
+ips=['127.0.0.1']
+ports=[19200]#, 19201] # a common baud rate XD
+
+
+save_main=False# save main blockchain?
+save_sub=False # save daughter blocks?
+full_node=True # run a full node?
+
 millis = lambda: int(round(time.time() * 1000))
 FirstBlock=None
 TopBlock=None
@@ -13,19 +21,23 @@ def HASH(data, bits=bits):
 myPublicKey=HASH(myPrivateKey.to_bytes(32, 'little'),128)
 print("Public Key: "+hex(myPublicKey))
 length=int(bits/8)
+current_data=""
+current_age=millis()
 class Block:
     def __init__(self, data, miner=0, lsblock=TopBlock, difficulty=None):
         global allblocks
         self.data=data
         self.lsblock=lsblock
-        self.difficulty=difficulty
         self.miner=miner
+        if difficulty!=None:
+            self.difficulty=min(int(((2**bits)-1)/difficulty),(2**bits)-1)
         if self.lsblock==None:
             self.lshash=0
-            self.difficulty=int((2**bits)/400000)
+            if difficulty==None:
+                self.difficulty=int((2**bits)/400000)
         else:
             self.lshash=self.lsblock.hash
-            if self.difficulty==None:
+            if difficulty==None:
                 self.difficulty=lsblock.difficulty
         self.time=int(time.time()) # EPOCH time!
         self.scratch=bytes(length)
@@ -45,6 +57,20 @@ class Block:
         self.hash_comp=self.getHash().to_bytes(length, 'little')
         self.result=self.hash_comp+self.gen_header()+bytes(self.data,"UTF-8")
         return self.result
+    def unpack(self,data):
+        self.hash=int.from_bytes(data[:length],'little')
+        data=data[length:]
+        self.lshash=int.from_bytes(data[:length],'little')
+        data=data[length:]
+        self.miner=int.from_bytes(data[:16],'little')
+        data=data[16:]
+        self.time=int.from_bytes(data[:8],'little')
+        data=data[8:]
+        self.difficulty=int.from_bytes(data[:length],'little')
+        data=data[length:]
+        self.scratch=data[:length]
+        self.data=data[length:].decode()
+        self.validate()
     def getHash(self):
         self.hash=HASH(self.gen_header()+bytes(self.data,"UTF-8"))
         return self.hash
@@ -69,6 +95,7 @@ def createBlock(newdata):
         return
     TopBlock=Block(newdata, lsblock=TopBlock)
     mine(TopBlock)
+    blocks.append(TopBlock)
     return TopBlock
 def createDaughterBlock(newdata,cmt):
     global TopBlock
@@ -77,9 +104,6 @@ def createDaughterBlock(newdata,cmt):
     createBlock(cmt+":DAUGHTER"+str(myPublicKey.to_bytes(16, 'little'))+hex(daughter.hash))
     TopBlock.daughter=daughter
     return daughter
-FirstBlock=createBlock("Who says data can't outlive its owners?")
-current_data=""
-current_age=millis()
 def addData(data, cmt='uu'):
     global current_age, current_data
     if len(current_data+data)<(4096-(3*length+28)):
@@ -96,4 +120,124 @@ def addData(data, cmt='uu'):
         createBlock(current_data)
         current_data=""
         current_age=millis()
-    
+def client_thread(cs):
+    index=0
+    while True:
+        data=b''
+        length=0
+        try:
+            tmp=cs.recv(8)
+            if not tmp:
+                break
+            elif tmp==b'PING':
+                data=b'PING'
+            else:
+                length= int.from_bytes(tmp,'little')
+        except:
+            break
+        while len(data)<length:
+            data+=cs.recv(min(8192,length-len(data)))
+        reply=b'DONE'
+        if data==b"PING":
+            print("Pinged.")
+            reply=b'OK'
+        elif data[:2]==b'I=':
+            data=data[2:]
+            index=int.from_bytes(data[:8],"little")
+            reply=b'DONE'
+            print("I set to "+str(index))
+        elif data==b"GBLK":
+            print("Block requested.")
+            if len(blocks)>index:
+                chunk=blocks[index].pack()
+            else:
+                chunk=b"NONE"
+            reply=chunk
+            reply=len(reply).to_bytes(8, 'little')+reply
+        #print("response start: "+reply.decode())
+        cs.sendall(reply)
+    print("A client has disconnected.")
+    cs.close()
+def start_new_thread(a, b):
+    what=threading.Thread(target=a, args=b)
+    what.start()
+def server(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind the socket to a public host, and a well-known port
+    try:
+        s.bind(('', port))
+        print("Node on port "+str(port)+" started.")
+    except:
+        print("Cannot run 2 nodes on same machine.")
+        return
+    # become a server socket
+    s.listen(5)
+    while True:
+        (cs, adr) = s.accept()
+        print("CONNECTED: "+adr[0]+':'+str(adr[1]))
+        start_new_thread(client_thread ,(cs,))
+class Client():
+    def __init__(self, ip, port):
+        self.ip=ip
+        self.conn=socket.socket()
+        self.conn.connect((ip, port))
+        self.conn.sendall(b"PING")
+        if self.conn.recv(10).decode()!='OK':
+            print("ERROR: "+str(ip)+" is not a node.")
+            self.conn.close()
+        else:
+            print("Node discovered: "+ip+':'+str(port))
+    def avsend(self, data):
+        self.conn.sendall(len(data).to_bytes(8, 'little'))
+        self.conn.sendall(data)
+    def avrec(self):
+        data=b''
+        length=0
+        tmp=self.conn.recv(8)
+        length=int.from_bytes(tmp,'little')
+        while len(data)<length:
+            data+=self.conn.recv(min(8192,length-len(data)))
+        return data
+    def getOneBlock(self):
+        self.avsend(b'I='+len(blocks).to_bytes(length,'little'))
+        if self.conn.recv(4)!=b'DONE':
+            print("ERROR: Node "+self.ip+" is no longer valid.")
+            return True
+        self.avsend(b'GBLK')
+        tmp=self.avrec()
+        if tmp==b'NONE':
+            return True
+        else:
+            blk=Block('')
+            blk.unpack(tmp)
+            blk.lshash=blocks[len(blocks)-1].hash
+            if blk.validate():
+                print("Downloaded block "+hex(blk.hash))
+                blocks.append(blk)
+        return False
+    def downloadAll(self):
+        print("Node "+self.ip+" downloading all blocks...")
+        while not self.getOneBlock():
+            pass
+        print("Node "+self.ip+" hasn't yet any more blocks.")
+FirstBlock=Block("Who says data can't outlive its owners?",difficulty=1, miner=hash(11))
+FirstBlock.scratch=bytes(length)
+blocks=[FirstBlock]
+nodes=[]
+for i in ips:
+    for x in ports:
+        sok=socket.socket()
+        try:
+            nodes.append(Client(i,x))
+        except:
+            pass
+if full_node:
+    for port in ports:
+        th=threading.Thread(target=server, args=(port,))
+        th.start()
+print("Downloading...")
+for i in nodes:
+    i.downloadAll()
+print("Done.")
+while True:
+    pass
