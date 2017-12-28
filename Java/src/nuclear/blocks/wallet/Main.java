a6c151c752c135d11a6d331537dc5a4a611b6666
NuclearManD <dylanbrophy@gmail.com>
2017-12-27 16:32:20 -0800

    Uploading works!

--- Java/src/nuclear/blocks/wallet/Main.java
+++ Java/src/nuclear/blocks/wallet/Main.java
@@ -1,15 +1,26 @@
 package nuclear.blocks.wallet;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Base64;
 
+import nuclear.blocks.client.ClientIface;
 import nuclear.blocks.wallet.ui.WalletGUI;
 import nuclear.slithercrypto.ECDSAKey;
+import nuclear.slithercrypto.blockchain.SavedChain;
 
 public class Main {
-	String keypath=System.getProperty("user.home")+"/AppData/Roaming/NuclearBlocks/keys/main.key";
+	String basepath=System.getProperty("user.home")+"/AppData/Roaming/NuclearBlocks";
+	String keypath=basepath+"/keys/main.key";
+	String blockchainStorePlace=basepath+"/blockchain/";
 	
 	ECDSAKey key;
+	
+	String nodeAdr="192.168.1.150";
+	
+	ClientIface iface;
+	SavedChain chain;
+	
 	public Main() {
 		if(new File(keypath).exists())
 			key=new ECDSAKey(keypath);
@@ -18,10 +29,18 @@ public class Main {
 			key=new ECDSAKey();
 			key.save(keypath);
 		}
-		WalletGUI gui=new WalletGUI();
-		gui.addressLabel.setText(Base64.getEncoder().encodeToString(key.getPublicKey()));
+		try {
+			iface=new ClientIface(nodeAdr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		chain=new SavedChain(blockchainStorePlace);
+		WalletGUI gui=new WalletGUI(chain,key,iface);
+		gui.addressLabel.setText("Address: "+Base64.getEncoder().encodeToString(key.getPublicKey()).replaceAll("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgA", "@"));
 		gui.coinCountLabel.setText("Please wait, connecting to network...");
 		
+		iface.downloadBlockchain(chain);
+		gui.coinCountLabel.setText("Balance: "+chain.getCoinBalance(key.getPublicKey())+" KiB");
 	}
 	
 	public static void main(String[] args) {
--- Java/src/nuclear/blocks/wallet/ui/WalletGUI.java
+++ Java/src/nuclear/blocks/wallet/ui/WalletGUI.java
@@ -5,53 +5,93 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JToolBar;
+
+import nuclear.blocks.client.ClientIface;
+import nuclear.slithercrypto.ECDSAKey;
+import nuclear.slithercrypto.blockchain.BlockchainBase;
+import nuclear.slithercrypto.blockchain.Transaction;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JList;
 import javax.swing.JTextPane;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
-public class WalletGUI extends JFrame {
+public class WalletGUI extends JFrame implements ActionListener{
 	public JToolBar toolBar;
 	public JLabel coinCountLabel;
 	public JLabel addressLabel;
-
-	public WalletGUI() {
+	
+	final JFileChooser fc = new JFileChooser();
+	BlockchainBase man;
+	
+	ECDSAKey key;
+	private ClientIface iface;
+	public WalletGUI(BlockchainBase man, ECDSAKey key,ClientIface iface) {
+		this.man=man;
+		this.key=key;
+		this.iface=iface;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(20,20,600,400);
+		setBounds(20,20,800,600);
 		getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 27, 584, 69);
+		panel.setBounds(0, 27, 784, 69);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		addressLabel = new JLabel("error");
-		addressLabel.setBounds(10, 11, 414, 21);
+		addressLabel.setBounds(10, 11, 764, 21);
 		panel.add(addressLabel);
 		
 		coinCountLabel = new JLabel("you have money!");
-		coinCountLabel.setBounds(10, 37, 414, 14);
+		coinCountLabel.setBounds(10, 37, 764, 21);
 		panel.add(coinCountLabel);
 		
 		toolBar = new JToolBar();
-		toolBar.setBounds(0, 0, 584, 23);
+		toolBar.setBounds(0, 0, 784, 23);
 		getContentPane().add(toolBar);
 		
-		JButton btnFile = new JButton("File");
-		btnFile.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		toolBar.add(btnFile);
+		JButton btnUpload = new JButton("Upload");
+		btnUpload.addActionListener(this);
+		btnUpload.setActionCommand("UPLOAD");
+		toolBar.add(btnUpload);
 		
-		JButton btnHelp = new JButton("Help");
-		btnHelp.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		toolBar.add(btnHelp);
+		JButton btnDownload = new JButton("Download");
+		btnDownload.setActionCommand("DOWNLOAD");
+		btnDownload.addActionListener(this);
+		toolBar.add(btnDownload);
 		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand()=="UPLOAD") {
+			JFileChooser jfc = new JFileChooser(System.getProperty("user.home"));
+			int retval=jfc.showOpenDialog(this);
+			if(retval==JFileChooser.APPROVE_OPTION) {
+				File file=jfc.getSelectedFile();
+				String path=file.getPath();
+				long length=file.length();
+				try {
+					FileInputStream stream=new FileInputStream(path);
+					byte[] buffer=new byte[(int) length];
+					for(int i=0;i<length;i++) {
+						buffer[i]=(byte) stream.read();
+					}
+					iface.uploadPair(Transaction.makeFile(key.getPublicKey(), key.getPrivateKey(), buffer, man.getBlockByIndex(man.length()-1).getHash(), file.getName()));
+					stream.close();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}else if(e.getActionCommand()=="DOWNLOAD") {
+			
+		}
 	}
 }
